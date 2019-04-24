04bb74ef1fe527eefb4044f74a572c3dd8c0b083
BryantFukushima <bmfukushima94@gmail.com>
2019-04-24 15:50:27 -0700

    bubble sort

---
+++ 02-sorting/01-bubble.js
@@ -0,0 +1,14 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubbleSort(array) {
+    for (let i = array.length; i > 0; i--) {
+        for (let j = 1; j < i; j++) {
+            if (array[j] < array[j - 1]) {
+                let temp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = temp;
+            }
+        }
+    }
+    return array;
+}
